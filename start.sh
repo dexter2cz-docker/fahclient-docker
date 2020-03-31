6c9f9c255760552ac53aa4cd9ac4dc5ba8be9f5d
dexter2cz-docker <54174755+dexter2cz-docker@users.noreply.github.com>
2020-03-31 14:26:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,16 +4,19 @@ ARGS="--run-as fahclient"
 
 MY_POWER=${POWER:-full}
 MY_USERNAME=${USERNAME:-Anonymous}
+MY_PASSKEY=${PASSKEY:-}
+MY_EXTRA=${EXTRA:-}
 # not empty or unset (-e TEAM= to set no team)
 [ ! "${TEAM+x$TEAM}" = "x" ] && MY_TEAM=${TEAM:=237170}
 MY_NVIDIA=${NVIDIA:-}
 
 ARGS="$ARGS --power=$MY_POWER --user=$MY_USERNAME"
 [ $MY_TEAM ] && ARGS="$ARGS --team=$MY_TEAM"
+[ $MY_PASSKEY ] && ARGS="$ARGS --passkey=$MY_PASSKEY"
+[ $MY_EXTRA ] && ARGS="$ARGS $MY_EXTRA"
 
 [ $MY_NVIDIA ] && [ ! -x /usr/bin/nvidia-smi ] && {
     # NVIDIA=440.64.00
-    #wget -q http://de.download.nvidia.com/XFree86/Linux-x86_64/440.64/NVIDIA-Linux-x86_64-440.64.run
     wget -q http://us.download.nvidia.com/tesla/$MY_NVIDIA/NVIDIA-Linux-x86_64-$MY_NVIDIA.run
     chmod +x NVIDIA-Linux-x86_64-$MY_NVIDIA.run
     ./NVIDIA-Linux-x86_64-$MY_NVIDIA.run -s --no-kernel-module
